342425a335f82a9fcefbadd6ac678c867d99434f
Andrey Belyakov <rayrah@live.com>
2023-04-08 00:25:28 +0100

    Create index template

---
+++ army_builder/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ArmyBuilderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'army_builder'
--- paradise_lost/urls.py
+++ paradise_lost/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from army_builder.views import get_index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", get_index, name="index")
 ]
