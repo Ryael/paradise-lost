320bffe4677fec391ea67b8f1d12233ea68486d0
Andrey Belyakov <rayrah@live.com>
2023-04-10 23:06:18 +0100

    Create Ruleset model

--- army_builder/models.py
+++ army_builder/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class BaseModel(models.Model):
+    """
+    Automatically adds timestamps and name fields to inherited models.
+    """
+    name = models.CharField(null=False, blank=False, max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Ruleset(BaseModel):
+    """
+    Stores ruleset data.
+    """
+
