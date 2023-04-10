e4fe088c716b02340e2cc62121886c3f7812cdd7
Andrey Belyakov <rayrah@live.com>
2023-04-11 00:37:28 +0100

    Move `Meta` class to top of model

--- army_builder/models.py
+++ army_builder/models.py
@@ -6,16 +6,15 @@ class BaseModel(models.Model):
     """
     Automatically adds timestamps and name fields to inherited models.
     """
+    class Meta:
+        abstract = True
+
     name = models.CharField(null=False, blank=False, max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        abstract = True
-
     def __str__(self):
         return self.name
-
 
 class Ruleset(BaseModel):
     """
@@ -26,10 +25,10 @@ class Army(BaseModel):
     """
     Stores army data.
     """
-    ruleset = models.ForeignKey(Ruleset, on_delete=models.CASCADE)
-
     class Meta:
         verbose_name_plural = "Armies"
+
+    ruleset = models.ForeignKey(Ruleset, on_delete=models.CASCADE)
 
 class Specialism(BaseModel):
     """
@@ -40,15 +39,15 @@ class Wargear(BaseModel):
     """
     Stores wargear statistics.
     """
+    class Meta:
+        verbose_name_plural = "Wargear"
+
     range = models.CharField(max_length=4)
     type = models.CharField(max_length=50)
     strength = models.CharField(max_length=3)
     armor_penetration = models.CharField(max_length=3)
     damage = models.CharField(max_length=5)
     points_cost = models.SmallIntegerField(default=0, null=False, blank=False)
-
-    class Meta:
-        verbose_name_plural = "Wargear"
 
 class Ability(BaseModel):
     """
