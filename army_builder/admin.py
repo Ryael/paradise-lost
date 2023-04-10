03a02322412e3950a020c0f396aebe1fa6523f6b
Andrey Belyakov <rayrah@live.com>
2023-04-11 00:35:21 +0100

    Expose Ability model to admin panel

--- army_builder/admin.py
+++ army_builder/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ruleset, Army, Unit, Specialism, Wargear
+from .models import Ruleset, Army, Unit, Specialism, Wargear, Ability
 
 # Register your models here.
 admin.site.register(Ruleset)
@@ -7,3 +7,4 @@ admin.site.register(Army)
 admin.site.register(Unit)
 admin.site.register(Specialism)
 admin.site.register(Wargear)
+admin.site.register(Ability)
