3cd6d20ee0087a51e5d450fc42432f660cb3827d
Andrey Belyakov <rayrah@live.com>
2023-04-11 00:01:24 +0100

    Expose Specialism model to admin panel

--- army_builder/admin.py
+++ army_builder/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Ruleset, Army, Unit
+from .models import Ruleset, Army, Unit, Specialism
 
 # Register your models here.
 admin.site.register(Ruleset)
 admin.site.register(Army)
 admin.site.register(Unit)
+admin.site.register(Specialism)
