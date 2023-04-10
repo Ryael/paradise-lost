e0fa67820c34d09706de9cd27ba499c006027119
Andrey Belyakov <rayrah@live.com>
2023-04-10 23:49:52 +0100

    Expose Unit model to admin panel

--- army_builder/admin.py
+++ army_builder/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Ruleset, Army
+from .models import Ruleset, Army, Unit
 
 # Register your models here.
 admin.site.register(Ruleset)
 admin.site.register(Army)
+admin.site.register(Unit)
