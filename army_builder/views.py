e973ee5be77f072ebe363ed7188f0588e2f846a0
Andrey Belyakov <rayrah@live.com>
2023-04-14 07:51:19 +0100

    Add function to display Roster model as html.

--- army_builder/views.py
+++ army_builder/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Roster
 
 # Create your views here.
 def get_index(request):
@@ -8,4 +9,11 @@ def get_dashboard(request):
     return render(request, "users/dashboard.html")
 
 def get_roster_list(request):
-    return render(request, "army_builder/rosters/view.html")
+
+    rosters = Roster.objects.all()
+
+    context = {
+        "rosters": rosters,
+    }
+
+    return render(request, "army_builder/rosters/view.html", context)
