1694f0c217f1420b6b40162a61725ac2e3d08b58
Andrey Belyakov <rayrah@live.com>
2023-04-17 00:52:57 +0100

    Add docstring.

--- army_builder/apps.py
+++ army_builder/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ArmyBuilderConfig(AppConfig):
+    """
+    This is the general application configuration.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'army_builder'
