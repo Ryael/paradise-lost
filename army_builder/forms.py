88a271b6bc3604b879a4eed5a1cc94387361be5c
Andrey Belyakov <rayrah@live.com>
2023-04-17 00:50:59 +0100

    Add docstring.

--- army_builder/forms.py
+++ army_builder/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Roster, Ruleset, Army
 
+
 class RosterForm(forms.ModelForm):
+    """
+    The basic form for the Roster model.
+    """
     class Meta:
         model = Roster
         fields = ["name", "ruleset", "army", "points_max"]
